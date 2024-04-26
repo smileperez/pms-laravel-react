d320586b168f495a3864fa46bd3127f233f46182
smileperez <aa.efimenko@efimen.ru>
2024-04-27 00:43:29 +0300

    Created migrations, models, factories and seeded database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +18,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Andrey Efimenko',
+            'email' => 'aa.efimenko@yandex.ru',
+            'password' => bcrypt('1Tr@v0lt@2'),
+            'email_verified_at' => time(),
         ]);
+
+        Project::factory()
+            ->count(30)
+            ->hasTasks(30)
+            ->create();
     }
 }
